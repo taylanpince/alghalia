c9b3a1854ca9bebca298e236ef146d888f1d97da
Taylan Pince <taylanpince@gmail.com>
2009-03-21 11:19:38 -0400

    Added Django-1.0.2 and django-command-extensions-r188, along with a barebones project folder.

---
+++ build/alghalia/settings.py
@@ -0,0 +1,75 @@
+import os
+
+
+gettext_noop = lambda s: s
+
+
+DEBUG = False
+TEMPLATE_DEBUG = DEBUG
+
+ADMINS = (
+    ('Taylan Pince', 'taylanpince@gmail.com'),
+)
+
+MANAGERS = ADMINS
+
+SITE_ID = 1
+
+USE_I18N = True
+
+LANGUAGE_CODE = 'en'
+
+LANGUAGES = (
+    ('ar', gettext_noop('Arabic')),
+    ('en', gettext_noop('English')),
+)
+
+SECRET_KEY = 'dc*zakji7&t*yz7=mv)ak*rqp17m&oa6w_-pc(ia&d14(il8b9'
+
+TIME_ZONE = 'Canada/Eastern'
+
+TEMPLATE_LOADERS = (
+    'django.template.loaders.filesystem.load_template_source',
+    'django.template.loaders.app_directories.load_template_source',
+)
+
+TEMPLATE_CONTEXT_PROCESSORS = (
+    'django.core.context_processors.auth',
+    'django.core.context_processors.debug',
+    'django.core.context_processors.i18n',
+    'django.core.context_processors.media',
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.middleware.common.CommonMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.middleware.doc.XViewMiddleware',
+)
+
+ROOT_URLCONF = 'urls'
+
+PROJECT_PATH = os.path.realpath(os.path.dirname(__file__))
+
+MEDIA_ROOT = os.path.join(PROJECT_PATH, 'media/')
+
+TEMPLATE_DIRS = (
+    os.path.join(PROJECT_PATH, 'templates/'),
+)
+
+INSTALLED_APPS = (
+    'django.contrib.admin',
+    'django.contrib.admindocs',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.sites',
+
+    'django_extensions',
+)
+
+try:
+    from settings_local import *
+except ImportError:
+    pass
