72946ca17f9cfa5595d4aa85a699e1af48b406ac
Taylan Pince <taylanpince@gmail.com>
2009-03-22 14:01:38 -0400

    Updated django settings file with the dev subdomain on MEDIA_PATH

--- conf/settings.py
+++ conf/settings.py
@@ -4,6 +4,6 @@ DATABASE_USER = 'alghaliadbu'
 DATABASE_PASSWORD = 'PtUhHaIdW0'
 DATABASE_HOST = '127.0.0.1'
 
-MEDIA_URL = 'http://alghalia.net/media/'
+MEDIA_URL = 'http://dev.alghalia.net/media/'
 
 ADMIN_MEDIA_PREFIX = MEDIA_URL + 'admin/'
