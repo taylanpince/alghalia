e2244b2bcf9c277694335b244e4ed60dce234333
Taylan Pince <taylanpince@gmail.com>
2009-03-25 20:04:00 -0400

    Added template tags to retrieve popular articles and featured categories

--- build/alghalia/articles/templatetags/article_tags.py
+++ build/alghalia/articles/templatetags/article_tags.py
@@ -1,6 +1,8 @@
+import re
+
 from django import template
 
-from articles.models import Article
+from articles.models import Article, ArticleCategory
 
 
 register = template.Library()
@@ -38,3 +40,50 @@ def latest_article(category=None):
         "category": category,
         "article": article,
     }
+
+
+@register.inclusion_tag("articles/includes/popular.html")
+def popular_articles(category=None):
+    """
+    Gets the most popular articles, optionally filtered by category
+    """
+    articles = Article.objects.all()
+
+    if category:
+        articles = Article.objects.filter(category=category)
+
+    articles = articles.order_by("-view_count")
+
+    return {
+        "category": category,
+        "articles": articles,
+    }
+
+
+class FeaturedCategoriesNode(template.Node):
+    def __init__(self, var_name):
+        self.var_name = var_name
+
+    def render(self, context):
+        categories = ArticleCategory.objects.filter(featured=True)
+
+        context[self.var_name] = categories
+
+        return ""
+
+
+@register.tag
+def get_featured_categories(parser, token):
+    try:
+        tag_name, arg = token.contents.split(None, 1)
+    except ValueError:
+        raise template.TemplateSyntaxError, "%r tag requires arguments" % token.contents.split()[0]
+
+    m = re.search(r"as (\w+)", arg)
+
+    if not m:
+        raise template.TemplateSyntaxError, "%r tag had invalid arguments" % tag_name
+
+    var_name, = m.groups()
+
+    return FeaturedCategoriesNode(var_name)
