95554391f8f3da5c8a08f48158a9de8ae96f027b
Taylan Pince <taylanpince@gmail.com>
2009-04-04 14:37:35 -0400

    Activated batchadmin for comments

--- build/alghalia/comments/admin.py
+++ build/alghalia/comments/admin.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import ugettext_lazy as _, ugettext
+
+from batchadmin.admin import BatchModelAdmin, CHECKBOX_NAME
 
 from comments.models import Comment
 
 
-class CommentAdmin(admin.ModelAdmin):
+class CommentAdmin(BatchModelAdmin):
     list_display = ("pk", "author", "email", "title", "body", "creation_date", "modification_date", "published", )
     list_filter = ["published", ]
     
@@ -24,6 +26,36 @@ class CommentAdmin(admin.ModelAdmin):
             "classes": ["collapse", ],
         }),
     )
+
+    batch_actions = ["delete_selected", "publish_selected", "unpublish_selected"]
+
+    def update_publication_status(self, request, changelist, status):
+        """
+        Updates the publication status of the selected items
+        """
+        selected = request.POST.getlist(CHECKBOX_NAME)
+        objects = changelist.get_query_set().filter(pk__in=selected)
+
+        for obj in objects:
+            obj.published = status
+            obj.save()
+
+    def publish_selected(self, request, changelist):
+        """
+        Publish selected items
+        """
+        self.update_publication_status(request, changelist, True)
+        self.message_user(request, ugettext("Selected comments have been published."))
+
+    def unpublish_selected(self, request, changelist):
+        """
+        Unpublish selected items
+        """
+        self.update_publication_status(request, changelist, False)
+        self.message_user(request, ugettext("Selected comments have been unpublished."))
+
+    publish_selected.short_description = _("Publish selected %(verbose_name_plural)s")
+    unpublish_selected.short_description = _("Unpublish selected %(verbose_name_plural)s")
 
 
 admin.site.register(Comment, CommentAdmin)
--- build/alghalia/settings.py
+++ build/alghalia/settings.py
@@ -72,6 +72,7 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.flatpages',
 
+    'batchadmin',
     'captcha',
     'django_extensions',
     'sharer',
